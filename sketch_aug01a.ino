3f78c37a0177c813a4d3ca97a0b5147d44d0ef8c
Andres Kwan <andreskwan@gmail.com>
2016-08-01 20:46:51 -0500

    p1 - [define an ISR]

---
+++ sketch_aug01a.ino
@@ -0,0 +1,20 @@
+int contador = 0;
+int n = contador; 
+
+void setup() {
+  Serial.begin(9600);
+  attachInterrupt(0,buttonCounter, FALLING);
+}
+
+void loop() {
+  //si cambio imprimir el valor del contador 
+  if (n != contador) {
+    Serial.println(contador);
+    n = contador;
+  }
+}
+
+void buttonCounter(){
+  contador++;
+}
+
