eeafd5bdaa17c99b4a4874c39910c21b1f5d1095
Andres Kwan <andreskwan@gmail.com>
2016-11-04 20:18:14 -0500

    001 - [Interrupciones] - [Prometec] - [codigo para validar funcionamiento de millis() cuando se tiene una interrupcion]

--- sketch_aug01a.ino
+++ sketch_aug01a.ino
@@ -1,20 +1,29 @@
 volatile int contador = 0;
 int n = contador; 
+long T0 = 0;
 
 void setup() {
   Serial.begin(9600);
-  attachInterrupt(0,buttonCounter, FALLING);
+  attachInterrupt(0,buttonCounter, LOW);
 }
 
 void loop() {
   //si cambio imprimir el valor del contador 
+  Serial.println("outside ISR before if n:"+ n);
   if (n != contador) {
+    Serial.println(millis());
     Serial.println(contador);
+    Serial.println(T0);
+    Serial.println(millis());
     n = contador;
   }
+  Serial.println("outside ISR after if n:"+ n);
 }
 
 void buttonCounter(){
-  contador++;
+  if(millis() > T0 + 2000) {
+    contador++;
+    T0 = millis(); 
+  }
 }
 
